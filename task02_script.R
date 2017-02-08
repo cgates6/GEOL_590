b8f79d4f871efe2979123466b5e1bd55de0376f8
Chris Gates <Cgates6@vols.utk.edu>
2017-02-08 11:19:38 -0500

    Task2

---
+++ task02_script.R
@@ -0,0 +1,22 @@
+my.variable <- 2+2
+my.variable/3
+
+####
+# This is an R script to demonstrate some basic operations in R
+####
+
+# Make three vectors 
+
+x <- seq(from=1, to=20, length.out=10)
+y <- rnorm(10)
+labels <- letters[10] 
+
+d <- data.frame(x, y, labels)
+
+# Save a plot
+png("test_plot.png", height=3, width=4, units="in", res=300)
+plot(x, y)
+dev.off()
+
+# Save a .csv file with the data
+write.csv(d,"c:/users/chris/desktop/GEOL_590/R/test_data.csv")
